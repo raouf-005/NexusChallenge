a6a0aa48d295a51ebcd24e2e4724432efb5ad2a5
raoufard <a_derardja@estin.dz>
2023-12-23 22:15:13 +0100

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar"
-
+import Rightbar from "./components/Rightbar"
 
 
 
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <div>
       <Navbar/>
+      <Rightbar/>
     </div>
   );
 }
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/rightbar.css";
+
+
+
+
+export default function Rightbar() {
+    return(
+        <div className="rightbar">
+            <h1>hey there</h1>
+        </div>
+
+    )
+}
