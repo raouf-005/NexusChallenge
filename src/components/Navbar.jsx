c6de478061ec95a2e60f871780a12d7cdeb38807
raoufard <a_derardja@estin.dz>
2023-12-23 18:41:27 +0100

    sidebar styling almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,64 @@
-import react from 'react';
+import React, { createContext, useEffect } from 'react';
+import { useState } from 'react';
+import '../styles/Navbar.css'
+import Gear from '../assets/navbar/gear.svg'
+import Buildings from '../assets/navbar/Buildings.svg'
+import List from '../assets/navbar/List.svg'
+import Users from '../assets/navbar/Users.svg'
+import SignOut from '../assets/navbar/SignOut.svg'
+import Notepad from '../assets/navbar/Notepad.svg'
+import SyphaxLogo from '../assets/navbar/SyphaxLogo.svg'
+
+
+
+
+
+
+
+//i will use useContext later to pass the usestate props without passing them through the components
+const Button = (props) => {
+    
+
+    let buttonstyle = props.name === props.currentPage ? 'buttonCurrentpageStyling' : 'buttonStyling'
+    return (
+        <button
+            name={props.name}
+            className={buttonstyle}
+            onClick={() => {props.setCurrentPage(props.name)
+                console.log(props.name)
+                console.log(props.currentPage)}}
+        >
+            <img src={props.icon} alt="" />
+            {props.name}
+        </button>
+    )
+}
 
 
 
 
 export default function Navbar() {
-const [pages, setPages] = useState()
+const [currentPage, setCurrentPage] = useState('Dashboard')
+
+
+    
 
     return (
-        <nav>
-            <h1>hotel</h1>
-            <div>
-                <button>
-                    <img src="" alt="" />
-                    Dashboard
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    Booking
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    Users
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    Rooms
-                </button>
-            </div>
+        <nav className='sideNavBar'>
+                <img src={SyphaxLogo} alt="" className='LogoSyphax' />
+            
+                <div className='middleButtonsContainer'>
+                        <Button name="Dashboard" icon={List} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+                        <Button name="Booking" icon={Notepad} setCurrentPage={setCurrentPage} currentPage={currentPage} />
+                        <Button name="Users" icon={Users}  setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+                        <Button name="Rooms" icon={Buildings}  setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+                </div>
 
-            <div>
-                <button>
-                    <img src="" alt="" />
-                    Settings
-                </button>
-                <button>
-                    <img src="" alt="" />
-                    Log Out
-                </button>
-            </div>
+                <div className='bottomContainer'>
+                    <Button name="Settings" icon={Gear} />
+                    <Button name="Log Out" icon={SignOut} />
+                </div>
+           
         </nav>
     )
-
-
 }
